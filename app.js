ddfd7497a85807dde905a6df5fc24ed52b1b092d
Amatisme <publicmat@me.com>
2020-03-20 22:13:17 -0400

    update for 0322020

--- app.js
+++ app.js
@@ -21,3 +21,27 @@ function toggleTheme() {
   // set local storage to save
   localStorage.setItem('theme', next);
 }
+
+// get new entries from data.json
+let entries = JSON.parse(data);
+
+//iterate entries and append to DOM
+entries.forEach((item, i) => {
+   // create new elements
+   var node = document.createElement("content");
+   var label = document.createElement("label");
+   var code = document.createElement("code");
+
+   // create text nodes and populate content
+   let labelText = document.createTextNode("> run " + item.label);
+   let codeText = document.createTextNode(item.code);
+
+   //apend new text
+   label.appendChild(labelText);
+   code.appendChild(codeText);
+
+   //append new elements
+   node.appendChild(label);
+   node.appendChild(code);
+   document.getElementById("entries").appendChild(node);
+ });
